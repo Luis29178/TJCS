d4278acda5901bb7d66354fb16b214116d1faaf8
Luis Bazaldua <Luis29178@gmail.com>
2022-03-20 15:26:14 -0400

    MapView Done Paintciew (constructor built)

--- app/src/main/java/com/example/tjcsprototype/TJCP.kt
+++ app/src/main/java/com/example/tjcsprototype/TJCP.kt
@@ -1,9 +1,21 @@
 package com.example.tjcsprototype
 
+import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class TJCP : AppCompatActivity() {
+
+    val MainMap = findViewById(R.id.CostumsMap) as ImageView
+
+    companion object{
+        var path = Path()
+        var brush = Paint()
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_pathing)
