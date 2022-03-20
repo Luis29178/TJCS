83522d34880daeb9dff6ea6f84c307cae484f57c
Luis Bazaldua <Luis29178@gmail.com>
2022-03-20 14:34:32 -0400

    view bult resorce error on activity_map_pathing.xml

---
+++ app/src/main/java/com/example/tjcsprototype/TJCP.kt
@@ -0,0 +1,12 @@
+package com.example.tjcsprototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TJCP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_pathing)
+
+    }
+}
